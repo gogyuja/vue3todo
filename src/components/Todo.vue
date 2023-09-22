708af1d0a71c4bb1138642e590f6dc342b26f767
gogyuja <gogyuja@gmail.com>
2023-09-22 19:05:52 +0900

    Todo 전체창 클릭시 체크박스 클릭 및 체크여부에 따른 취소선 작업 중

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,26 @@
 <template>
-    <li class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded" style="background-color: #f4f6f7;">
-        <div class="d-flex align-items-center">
-            <input id="{{ todo.idx }}" class="form-check-input me-2" type="checkbox" value="" aria-label="..." checked />
-            <s>{{todo.title}}</s>
+    <li class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded" @click="clickCheckbox()" style="background-color: #f4f6f7;">
+        <div class="d-flex align-items-center" @click.capture="">
+            <input ref="checkboxRef" :id="todo.idx" class="form-check-input me-2" type="checkbox" value="" aria-label="..." checked @click.stop />
+            <div contenteditable="true" @click.stop=""><span><s :style="isVisible ? {} : { display: 'contents' }">{{ todo.title }}</s></span></div>
         </div>
         <span>
-            <i :class="classObject" class="bi" @mouseover="onHover" @mouseout="onHoverOut" @click="$emit('delete-todo',todo)"></i>
+            <i :class="classObject" class="bi" @mouseover="onHover" @mouseout="onHoverOut" @click.stop="$emit('delete-todo',todo)"></i>
         </span>
     </li>
 </template>
 
 <script setup lang='ts'>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
     defineProps(['todo'])
 
     const classObject = reactive({
         'bi-trash': true,
         'bi-trash-fill': false
     })
+
+    const checkboxRef = ref<null | HTMLInputElement>(null);
+    const isVisible = ref(false);
 
     const onHover = () => {
         classObject['bi-trash'] = false;
@@ -28,10 +31,11 @@ import { reactive } from 'vue'
         classObject['bi-trash'] = true;
         classObject['bi-trash-fill'] = false;
     }
-</script>
-    
-<style lang="scss">
-    i {
-        cursor: pointer;
+
+    const clickCheckbox=()=>{       
+        // 체크박스의 체크 상태를 토글합니다.
+        if(checkboxRef.value) {
+            checkboxRef.value.checked = !checkboxRef.value.checked;
+        }
     }
-</style>
+</script>
